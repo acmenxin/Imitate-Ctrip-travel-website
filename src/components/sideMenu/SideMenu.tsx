cb4b362f2a99291a08f3eaee35093ab98e6478f6
郭佳欣 <acmenxin@163.com>
2022-12-03 22:45:41 +0800

    feat: 轮播图与侧边栏

---
+++ src/components/sideMenu/SideMenu.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./SideMenu.module.css";
+import { sideMenuList } from "./mockup";
+import { Menu } from "antd";
+import { HeartOutlined } from "@ant-design/icons";
+
+export const SideMenu: React.FC = () => {
+  return (
+    <Menu mode="vertical" className={styles["side-menu"]}>
+      {sideMenuList.map((m, index) => (
+        <Menu.SubMenu
+          key={`side-menu-${index}`}
+          title={
+            <span>
+              <HeartOutlined />
+              {m.title}
+            </span>
+          }
+        >
+          {m.subMenu.map((sm, smindex) => (
+            <Menu.SubMenu
+              key={`sub-menu-${smindex}`}
+              title={
+                <span>
+                   <HeartOutlined />
+                  {sm.title}
+                </span>
+              }
+            >
+              {sm.subMenu.map((sms, smsindex) => (
+                <Menu.Item key={`sub-sub-menu-${smsindex}`}>
+                  <span>
+                  <HeartOutlined />
+                    {sms}
+                  </span>
+                </Menu.Item>
+              ))}
+            </Menu.SubMenu>
+          ))}
+        </Menu.SubMenu>
+      ))}
+    </Menu>
+  );
+};
